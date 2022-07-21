41f16995b0d6b1cf66ef57065c4295760dfbb7a2
Sarahdeep <qwerty200468@gmail.com>
2022-07-21 19:08:53 +0300

    Поправил работу метода получения всех баллов

--- src/main/java/com/example/springdemo/service/GradesListService.java
+++ src/main/java/com/example/springdemo/service/GradesListService.java
@@ -4,12 +4,15 @@ import com.example.springdemo.repository.GradesListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class GradesListService {
     @Autowired
     private GradesListRepository gradesListRepository;
 
     public int getTotalPointsByStudentId(Long id) {
-        return gradesListRepository.getTotalPointsByStudentId(id);
+        Optional<Integer> totalPoints = gradesListRepository.getTotalPointsByStudentId(id);
+        return totalPoints.orElse(0);
     }
 }
