e8fa7d9c4c9e4bc673e04b98c1a08d8f1869ae3c
Sarahdeep <qwerty200468@gmail.com>
2022-07-28 00:31:22 +0300

    Added auth&registration for users

--- src/main/java/com/example/springdemo/config/WebSecurityConfig.java
+++ src/main/java/com/example/springdemo/config/WebSecurityConfig.java
@@ -1,24 +1,30 @@
 package com.example.springdemo.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/static/**","/register").permitAll()
+                .antMatchers("/","/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
                     .formLogin()
@@ -29,24 +35,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll();
     }
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
     @Override
     public void configure(WebSecurity web) {
         web.ignoring()
                 .antMatchers(
-                        "/css/**", "/fonts/**","/font-awesome","/js",
+                        "/css/**", "/fonts/**","/font-awesome/**","/js/**",
                         "/img/**");
+    }
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .usersByUsernameQuery("select username,password,active from users where username=?")
+                .authoritiesByUsernameQuery("select users.username,role.roles from users  inner join role  on users.user_id=role.user_id where users.username=?");
     }
 }
 
--- src/main/java/com/example/springdemo/entity/Role.java
+++ src/main/java/com/example/springdemo/entity/Role.java
@@ -6,25 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
-@Entity
-@Table(
-        name = "role"
-)
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(
-            name = "role_id"
-    )
-    private Long id;
-
-    @Column(
-            name = "role",
-            length = 30
-    )
-    private String role;
+public enum Role {
+    USER;
 }
