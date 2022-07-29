a142e36a08b0a3d7db54b4ebb907711e4369d42a
makezh <max.kezhaev@gmail.com>
2022-07-29 04:47:43 +0300

    fix format message

--- src/main/java/com/example/springdemo/service/UserServiceImpl.java
+++ src/main/java/com/example/springdemo/service/UserServiceImpl.java
@@ -58,11 +58,11 @@ public class UserServiceImpl implements UserService {
 
         String message = String.format(
                 "Hello, %s!\n" +
+                        "\n" +
                         "We are glad to welcome you to the LabSpace!\n" +
                         "\n" +
                         "Please, follow the link to verify your profile:\n" +
-                        "\n" +
-                        "http://iu9.yss.su:8080/activate/%s",
+                        "http://localhost:8080/activate/%s",  // CHANGE TO "iu9.yss.su:8080/activate/%s"
                 user.getFirstName(),
                 user.getActivationCode()
         );
