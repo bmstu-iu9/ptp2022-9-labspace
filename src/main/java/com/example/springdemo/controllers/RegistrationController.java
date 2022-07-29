268c593eb95709ffbe54751e52cc13db283edb0f
makezh <max.kezhaev@gmail.com>
2022-07-29 04:31:36 +0300

    add activated message

--- src/main/java/com/example/springdemo/controllers/RegistrationController.java
+++ src/main/java/com/example/springdemo/controllers/RegistrationController.java
@@ -51,13 +51,13 @@ public class RegistrationController {
     @GetMapping("/activate/{code}")
     public String activate(Model model, @PathVariable String code) {
         boolean isActivated = userService.activateUser(code);
-//
-//        if (isActivated) {
-//            model.addAttribute("message", "User successfully activated!");
-//        } else {
-//            model.addAttribute("message", "Activation code is not found!");
-//        }
-//
+
+        if (isActivated) {
+            model.addAttribute("message", "User successfully activated!");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+
        return "/login";
     }
 }
