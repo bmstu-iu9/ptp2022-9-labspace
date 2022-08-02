d92bdf627aa80307b6ba7a3fdcc5ba7abd00013c
Goose-bump <qwerty200468@gmail.com>
2022-08-02 15:07:01 +0300

    added view for uploading files

--- src/main/java/com/example/springdemo/controllers/FileUploadController.java
+++ src/main/java/com/example/springdemo/controllers/FileUploadController.java
@@ -3,12 +3,15 @@ package com.example.springdemo.controllers;
 import com.example.springdemo.response.UploadResponse;
 import com.example.springdemo.service.FileStorageService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+@Controller
 public class FileUploadController {
     private final FileStorageService fileStorageService;
 
@@ -17,13 +20,16 @@ public class FileUploadController {
     }
 
     @PostMapping("/upload")
-    public ResponseEntity<UploadResponse> uploadFile(
-            @RequestParam(name = "file", required = false) MultipartFile file
-    ) {
+    public String uploadFile(
+            @RequestParam(name = "file", required = false) MultipartFile file, RedirectAttributes redirectAttributes) {
         String fileName = fileStorageService.storeFile(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
 
-        UploadResponse uploadResponse = new UploadResponse(fileName);
-
-        return ResponseEntity.ok().body(uploadResponse);
+        return "redirect:/upload";
+    }
+    @GetMapping("/upload")
+    public String view(){
+        return "lab";
     }
 }
