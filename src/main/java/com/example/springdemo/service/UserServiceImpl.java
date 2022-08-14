071504b3a18be007d95bb151586bdc0aedaa1364
marcheanin <marcheanin1576@gmail.com>
2022-08-15 00:35:26 +0300

    add deploy links

--- src/main/java/com/example/springdemo/service/UserServiceImpl.java
+++ src/main/java/com/example/springdemo/service/UserServiceImpl.java
@@ -63,7 +63,7 @@ public class UserServiceImpl implements UserService {
                         "We are glad to welcome you to the LabSpace!\n" +
                         "\n" +
                         "Please, follow the link to verify your profile:\n" +
-                        "http://localhost:8080/auth/activate/%s",
+                        "http://iu9.yss.su/auth/activate/%s",
                 user.getFirstName(),
                 user.getActivationCode()
         );
