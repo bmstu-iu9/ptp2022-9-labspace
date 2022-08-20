8ad988b097904964b17cc0a95500e7f044039682
Time010302Skipper <sasha.shvech2003@mail.ru>
2022-08-20 21:58:16 +0300

    Upload lab
    Create template of uploading done labs

--- src/main/java/com/example/springdemo/controllers/MainController.java
+++ src/main/java/com/example/springdemo/controllers/MainController.java
@@ -88,4 +88,10 @@ public class MainController {
         addNameAndGroupToModel(model);
         return "teacher_lab";
     }
+
+    @GetMapping("/main/upload_lab")
+    public String upload_lab(HttpServletRequest request, Model model) {
+        addNameAndGroupToModel(model);
+        return "templs/templateOfUploadLab";
+    }
 }
