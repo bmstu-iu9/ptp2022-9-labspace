32d4e6504969728b95e202d82ca2b13329ccb27f
Sarah_deep <qwerty200468@gmail.com>
2022-08-21 16:17:26 +0300

    labUploadForm linked with model

--- src/main/java/com/example/springdemo/controllers/LabInfoUploadController.java
+++ src/main/java/com/example/springdemo/controllers/LabInfoUploadController.java
@@ -1,14 +1,17 @@
 package com.example.springdemo.controllers;
 
-import com.example.springdemo.entity.Course;
-import com.example.springdemo.entity.Groupp;
-import com.example.springdemo.entity.LabInfo;
-import com.example.springdemo.entity.User;
+import com.example.springdemo.entity.*;
 import com.example.springdemo.repository.CourseRepository;
+import com.example.springdemo.repository.DeadlineRepository;
 import com.example.springdemo.repository.LabInfoRepository;
+import com.example.springdemo.service.DeadlineService;
 import com.example.springdemo.service.FileStorageService;
 import com.example.springdemo.service.LabInfoService;
+import com.example.springdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,7 +26,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -34,24 +40,54 @@ public class LabInfoUploadController {
     private FileStorageService fileStorageService;
     @Autowired
     LabInfoService labInfoService;
-    @GetMapping(value="/uploadLab")
-    public String uploadlab(Model model){
-        LabInfo labInfo=new LabInfo();
-        model.addAttribute("labInfo", labInfo);
-        return "uploadLab";
+    @Autowired
+    UserService userService;
+    @Autowired
+    DeadlineService deadlineService;
+    public String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            return authentication.getName();
+        }
+        return "guest";
     }
 
-    @PostMapping(value = "/uploadLab")
+    public void addNameAndGroupToModel(Model model){
+        String username;
+        username = getCurrentUsername();
+        if (!Objects.equals(username, "guest")) {
+            User user = userService.getByEmail(username);
+            model.addAttribute("name", user.getFirstName() + " " + user.getLastName());
+            model.addAttribute("groupp",user.getGroupp().getName());
+        }
+        else{
+            model.addAttribute("name", "guest");
+            model.addAttribute("groupp", "");
+        }
+    }
+
+
+    @GetMapping(value="/main/upload_lab")
+    public String uploadlab(Model model){
+        LabInfo labInfo=new LabInfo();
+        addNameAndGroupToModel(model);
+        model.addAttribute(courseRepository);
+        model.addAttribute("labInfo", labInfo);
+        return "teacher_lab";
+    }
+
+    @PostMapping(value = "/main/upload_lab")
     public String regUser( @Valid LabInfo labInfo,
                           @RequestParam(name = "filee") MultipartFile file, RedirectAttributes redirectAttributes,
                           Model model,
-                          HttpServletRequest request) throws ServletException {
+                          HttpServletRequest request) throws ServletException, ParseException {
             labInfo.setUploadDate(new Date(System.currentTimeMillis()));
             Optional<Course> tmpcourse = courseRepository.findById(Long.valueOf(request.getParameter("course_id")));
             labInfo.setSource("labs/");
             fileStorageService.storeFile(file,labInfo);
             tmpcourse.ifPresent(labInfo::setCourse);
             labInfoService.uploadLab(labInfo);
-        return "uploadLab";
+            deadlineService.saveDeadlines(request,labInfo);
+        return "teacher_lab";
     }
 }
