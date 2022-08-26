e109e80d47355e4e11afbcf26f52e403ee6b311b
SanyakaMLG <alekseev.sasha0204@yandex.ru>
2022-08-26 19:25:17 +0300

    add method to get number of variant

--- src/main/java/com/example/springdemo/repository/VariantRepository.java
+++ src/main/java/com/example/springdemo/repository/VariantRepository.java
@@ -4,6 +4,9 @@ import com.example.springdemo.entity.Variant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VariantRepository extends JpaRepository<Variant, Long> {
+    Optional<Integer> findByLabInfoIdAndStudentId(Long labInfoId, Long studentId);
 }
