7fac4692caad5c730269aeadd0bdfe419d0b60b0
SanyakaMLG <alekseev.sasha0204@yandex.ru>
2022-08-26 17:53:13 +0300

    codestyle fixes

--- src/main/java/com/example/springdemo/service/LabInfoServiceImpl.java
+++ src/main/java/com/example/springdemo/service/LabInfoServiceImpl.java
@@ -1,23 +1,20 @@
 package com.example.springdemo.service;
 
-import com.example.springdemo.entity.Deadline;
-import com.example.springdemo.entity.Groupp;
 import com.example.springdemo.entity.LabInfo;
 import com.example.springdemo.repository.DeadlineRepository;
 import com.example.springdemo.repository.LabInfoRepository;
 import com.example.springdemo.repository.UserRepository;
-import com.example.springdemo.service.LabInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.*;
 
 @Service
 public class LabInfoServiceImpl implements LabInfoService {
     @Autowired
     private DeadlineRepository deadlineRepository;
+
     @Autowired
     private LabInfoRepository labInfoRepository;
+    
     @Autowired
     private UserRepository userRepository;
 
