561dd00c62426d327b950cbf761202befc8dbc20
SanyakaMLG <alekseev.sasha0204@yandex.ru>
2022-08-31 18:42:41 +0300

    add method to submit lab repository

--- src/main/java/com/example/springdemo/repository/SubmitLabRepository.java
+++ src/main/java/com/example/springdemo/repository/SubmitLabRepository.java
@@ -2,6 +2,7 @@ package com.example.springdemo.repository;
 
 import com.example.springdemo.entity.SubmitLab;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -22,5 +23,8 @@ public interface SubmitLabRepository extends JpaRepository<SubmitLab, Long> {
     List<SubmitLab> findAll();
 
     List<SubmitLab> findAllByUserIdAndMarkGreaterThan(Long user_id, int mark);
+
+    @Query("select sl from SubmitLab sl where sl.user.id=?1 and sl.mark=-1")
+    List<SubmitLab> findAllByUserIdAndNotChecked(Long user_id);
 
 }
