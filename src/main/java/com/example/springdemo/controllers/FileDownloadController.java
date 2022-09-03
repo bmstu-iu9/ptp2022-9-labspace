47731fb21ebbe0fa77b98f5b7beefb824f188602
Sarah_deep <qwerty200468@gmail.com>
2022-09-03 12:46:17 +0300

    add view on new tab in browser for pdf files, lil bugfix

--- src/main/java/com/example/springdemo/controllers/FileDownloadController.java
+++ src/main/java/com/example/springdemo/controllers/FileDownloadController.java
@@ -5,6 +5,8 @@ import com.example.springdemo.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +15,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FileDownloadController {
-    @Autowired
-    private LabInfoRepository labInfoRepository;
 
     @Autowired
     private FileStorageService fileStorageService;
 
-    @GetMapping("main/labid{labId}/download/userid{userId}")
+    @GetMapping("main/lab_id{labId}/download/user_id{userId}")
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable Long labId, @PathVariable Long userId) {
 
         Resource file = fileStorageService.loadAsResource(labId, userId);
-        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.add("content-disposition", "inline;filename=" + file.getFilename());
+        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+        String fileName = file.getFilename();
+        if (fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0
+                && (fileName.substring(fileName.lastIndexOf(".") + 1).equals("pdf"))){
+            return new ResponseEntity<>(file,headers, HttpStatus.OK);
+        }else {
+            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                    "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+        }
     }
 }
